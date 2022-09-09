68f6bf82290497b129b4219d5184fe89cc2edb01
Danila Zinin <danilazinin@MacBook-Pro-Danila.local>
2022-09-09 22:06:52 +0300

    Версия0.2

---
+++ PrD5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 30: Напишите программу, которая выводит
+// массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+Console.Clear();
+int [] array = new int [8];
+
+void Rand(int [] arr)
+{
+    int length = arr.Length;
+    int index = 0;
+    Console.Write("[");
+    while (index < length-1)
+    //for (int i = 0; i < 8; i++)
+    {
+        arr[index] = new Random().Next(0,2);
+        Console.Write($"{arr[index]},");
+        index++;  
+    }
+        Console.Write($"{arr[index]}");
+
+    Console.Write("]");
+}
+Rand(array);
--- PrD6/Program.cs
+++ PrD6/Program.cs
@@ -6,10 +6,10 @@
 
 
 Console.Clear();
+Console.Write("[");
 void Massiv()
 {
 int[] array = new int[8];
-Console.Write("[");
 for (int i = 0; i < array.Length; i++)
    {
     array[i] = new Random().Next(0, 2);
